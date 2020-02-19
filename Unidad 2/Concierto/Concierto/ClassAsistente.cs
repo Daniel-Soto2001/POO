2f99edf02c7ae23af9a403cbeeddfc324898ccd6
Daniel-Soto2001 <dani500120@gmail.com>
2020-02-18 17:29:46 -0700

    Unidad 2

---
+++ Unidad 2/Concierto/Concierto/ClassAsistente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concierto
+{
+   public class ClassAsistente       
+    {
+        //Atributos de la clase
+        public int bebes=0, niños=0, adultos=0, terceraedad=0;
+        public int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+        public string personas;
+         //Metodos de la clase
+        public void Registro()
+        {
+       
+            switch (personas)
+                {
+                case "Bebes":
+                    bebes = bebes + num1;
+                    break;
+                case "Niños":
+                    niños = niños + num2;
+                    break;
+                case "Adultos":
+                    adultos = adultos + num3;
+                    break;
+                case "Tercera edad":
+                    terceraedad = terceraedad + num4;
+                    break;
+                }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Unidad 2/Concierto/Concierto/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Concierto
+{
+
+    public partial class frmConcierto : Form
+    {
+        ClassAsistente objConcierto = new ClassAsistente();
+
+        public frmConcierto()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            objConcierto.personas = cmbPersonas.Text.ToString();
+            objConcierto.num1 = int.Parse(cmbCuenta.Text.ToString());
+            objConcierto.num2 = int.Parse(cmbCuenta.Text.ToString());
+            objConcierto.num3 = int.Parse(cmbCuenta.Text.ToString());
+            objConcierto.num4 = int.Parse(cmbCuenta.Text.ToString());
+            objConcierto.Registro();
+            cmbCuenta.Text = "";
+            cmbPersonas.Text = "";
+        }
+
+
+        private void frmConcierto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = objConcierto.bebes.ToString();
+            textBox2.Text = objConcierto.niños.ToString();
+            textBox3.Text = objConcierto.adultos.ToString();
+            textBox4.Text = objConcierto.terceraedad.ToString();
+
+
+        }
+
+    }
+}
