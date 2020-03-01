7664e2be59b21ce1043e40563cda42b541b1d949
Daniel-Soto2001 <dani500120@gmail.com>
2020-02-29 19:49:37 -0700

    Actualizar2

---
+++ U23POO Daniel_Elias/Examen U23/Examen2y3/MayorEdad/ClassPersona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorEdad
+{
+    class ClassPersona
+    {
+        //Atributos de la clase
+        public int anoActual, MesActual, DiaActual;
+        public int anoNacimiento, MesNacimiento, DiaNacimiento;
+        public string MayorEdad="";
+        public string MenorEdad = "";
+       
+        //Metodos de la clase
+        public void MayordeEdad()
+        {
+            if (anoActual - anoNacimiento > 18)
+            {
+                MayorEdad = "Es mayor de Edad";
+                MenorEdad = "Es menor de Edad";
+
+            }
+            else if (anoActual - anoNacimiento == 18)
+            {
+                if (MesActual > MesNacimiento)
+                {
+                    MayorEdad = "Es mayor de Edad";
+                }
+
+                else if (MesActual == MesNacimiento)
+                {
+                    if (DiaActual > DiaNacimiento)
+                    {
+                        MayorEdad = "Es mayor de Edad";
+                    }
+                    else if(DiaActual==DiaNacimiento)
+                    {
+                        MayorEdad = "Es mayor de Edad";
+                    }
+                }
+            }
+
+
+
+            
+
+          }
+        }
+    }
---
+++ U23POO Daniel_Elias/Examen U23/Examen2y3/MayorEdad/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayorEdad
+{
+    public partial class Form1 : Form
+    {
+        ClassPersona objPersona = new ClassPersona();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            objPersona.anoActual = int.Parse(DateTime.Today.Year.ToString());
+            objPersona.MesActual = int.Parse(DateTime.Today.Month.ToString());
+            objPersona.DiaActual = int.Parse(DateTime.Today.Day.ToString());
+            objPersona.anoNacimiento = int.Parse(dtpFechas.Value.Year.ToString());
+            objPersona.MesNacimiento = int.Parse(dtpFechas.Value.Month.ToString());
+            objPersona.DiaNacimiento = int.Parse(dtpFechas.Value.Day.ToString());
+            objPersona.MayordeEdad();
+            lblMayor.Text = objPersona.MayorEdad.ToString();
+            objPersona.MayorEdad = "";
+            objPersona.MenorEdad = "";
+
+
+
+
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
