499d78370d4fca8894800bb5650046b8dd3ee6ea
Daniel-Soto2001 <dani500120@gmail.com>
2020-05-15 21:24:57 -0600

    ge

---
+++ UNIDAD 5/Calcularadora ProyectoFinal/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Calcularadora_ProyectoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFila1 = new System.Windows.Forms.TextBox();
+            this.txtColumna1 = new System.Windows.Forms.TextBox();
+            this.txtColumna2 = new System.Windows.Forms.TextBox();
+            this.txtFila2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.btnInsertar2 = new System.Windows.Forms.Button();
+            this.btnMostrar1 = new System.Windows.Forms.Button();
+            this.btnMostrar2 = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(77, 13);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 0;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(64, 42);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(240, 150);
+            this.dgv1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fila";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Columna";
+            // 
+            // txtFila1
+            // 
+            this.txtFila1.Location = new System.Drawing.Point(12, 42);
+            this.txtFila1.Name = "txtFila1";
+            this.txtFila1.Size = new System.Drawing.Size(39, 20);
+            this.txtFila1.TabIndex = 4;
+            // 
+            // txtColumna1
+            // 
+            this.txtColumna1.Location = new System.Drawing.Point(12, 93);
+            this.txtColumna1.Name = "txtColumna1";
+            this.txtColumna1.Size = new System.Drawing.Size(39, 20);
+            this.txtColumna1.TabIndex = 5;
+            // 
+            // txtColumna2
+            // 
+            this.txtColumna2.Location = new System.Drawing.Point(326, 93);
+            this.txtColumna2.Name = "txtColumna2";
+            this.txtColumna2.Size = new System.Drawing.Size(39, 20);
+            this.txtColumna2.TabIndex = 11;
+            // 
+            // txtFila2
+            // 
+            this.txtFila2.Location = new System.Drawing.Point(326, 42);
+            this.txtFila2.Name = "txtFila2";
+            this.txtFila2.Size = new System.Drawing.Size(39, 20);
+            this.txtFila2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Columna";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(326, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fila";
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(378, 42);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.Size = new System.Drawing.Size(240, 150);
+            this.dgv2.TabIndex = 7;
+            // 
+            // btnInsertar2
+            // 
+            this.btnInsertar2.Location = new System.Drawing.Point(391, 13);
+            this.btnInsertar2.Name = "btnInsertar2";
+            this.btnInsertar2.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar2.TabIndex = 6;
+            this.btnInsertar2.Text = "Insertar";
+            this.btnInsertar2.UseVisualStyleBackColor = true;
+            this.btnInsertar2.Click += new System.EventHandler(this.btnInsertar2_Click);
+            // 
+            // btnMostrar1
+            // 
+            this.btnMostrar1.Location = new System.Drawing.Point(64, 198);
+            this.btnMostrar1.Name = "btnMostrar1";
+            this.btnMostrar1.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar1.TabIndex = 12;
+            this.btnMostrar1.Text = "Mostrar";
+            this.btnMostrar1.UseVisualStyleBackColor = true;
+            this.btnMostrar1.Click += new System.EventHandler(this.btnMostrar1_Click);
+            // 
+            // btnMostrar2
+            // 
+            this.btnMostrar2.Location = new System.Drawing.Point(378, 198);
+            this.btnMostrar2.Name = "btnMostrar2";
+            this.btnMostrar2.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar2.TabIndex = 13;
+            this.btnMostrar2.Text = "Mostrar";
+            this.btnMostrar2.UseVisualStyleBackColor = true;
+            this.btnMostrar2.Click += new System.EventHandler(this.btnMostrar2_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(55, 227);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.Size = new System.Drawing.Size(240, 150);
+            this.dgvResultados.TabIndex = 14;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(55, 383);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(75, 23);
+            this.btnSuma.TabIndex = 15;
+            this.btnSuma.Text = "Sumar";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(136, 383);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(75, 23);
+            this.btnRestar.TabIndex = 16;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(220, 383);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 17;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(510, 378);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(83, 32);
+            this.btnSalir.TabIndex = 18;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 424);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnMostrar2);
+            this.Controls.Add(this.btnMostrar1);
+            this.Controls.Add(this.txtColumna2);
+            this.Controls.Add(this.txtFila2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.btnInsertar2);
+            this.Controls.Add(this.txtColumna1);
+            this.Controls.Add(this.txtFila1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnInsertar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFila1;
+        private System.Windows.Forms.TextBox txtColumna1;
+        private System.Windows.Forms.TextBox txtColumna2;
+        private System.Windows.Forms.TextBox txtFila2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Button btnInsertar2;
+        private System.Windows.Forms.Button btnMostrar1;
+        private System.Windows.Forms.Button btnMostrar2;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ UNIDAD 5/Calcularadora ProyectoFinal/Operaciones.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Calcularadora_ProyectoFinal
+{
+    class Operaciones
+    {
+        public int Fila1 { get; set; }
+        public int Fila2 { get; set; }
+        public int Columna1 { get; set; }
+        public int Columna2 { get; set; }
+
+        int [,] MatrizA = new int [100, 100];
+        int [,] MatrizB = new int [100, 100];
+        int [,] Resultado = new int [100,100];
+        int i, j, z;
+
+        public void InsertarA(TextBox Pantalla1, TextBox Pantalla2)
+        {
+            if(Pantalla1.Text==""|| Pantalla2.Text=="")
+            {
+                MessageBox.Show("Ingrese Datos");
+            }
+            else
+            {
+                Fila1 = int.Parse(Pantalla1.Text);
+                Columna1 = int.Parse(Pantalla2.Text);
+
+                for(i=0; i<Columna1; i++)
+                {
+                    for(j=0; j<Columna1; j++)
+                    {
+                        MatrizA[i, j] = int.Parse(Interaction.InputBox("Ingresa un numero", "Ingrese otro numero","", Fila1, Columna1));
+                            
+                    }
+                }
+            }
+        }
+
+        public void InsertarB(TextBox Pantalla3, TextBox Pantalla4)
+        {
+            if(Pantalla3.Text=="" || Pantalla4.Text=="")
+            {
+                MessageBox.Show("Ingrese Datos");
+            }
+            else
+            {
+                Fila2 = int.Parse(Pantalla3.Text);
+                Columna2 = int.Parse(Pantalla4.Text);
+
+                for(i=0; i<Fila2; i++)
+                {
+                    for(j=0; j<Columna2; j++)
+                    {
+                        MatrizB[i, j] = int.Parse(Interaction.InputBox("Ingrese numero", "Ingrese otro numero", "", Fila2, Columna2));
+                    }
+                }
+            }
+        }
+
+        public void MostrarA(DataGridView Tabla)
+        {
+            Tabla.RowCount = Fila1;
+            Tabla.ColumnCount = Columna1;
+
+            for(i=0; i<Fila1; i++)
+            {
+                for(j=0; j<Columna1; j++)
+                    {
+                    Tabla.Rows[i].Cells[j].Value = MatrizA[i,j].ToString();
+                    }
+            }
+        }
+
+        public void MostrarB(DataGridView Tabla)
+        {
+            Tabla.RowCount = Fila2;
+            Tabla.ColumnCount = Columna2;
+
+            for (i = 0; i < Fila2; i++)
+            {
+                for (j = 0; j < Columna2; j++)
+                {
+                    Tabla.Rows[i].Cells[j].Value = MatrizB[i,j].ToString();
+                }
+            }
+        }
+        public void Suma(DataGridView Suma)
+        {
+            if (Fila1 == Fila2 && Columna1 == Columna2)
+            { 
+                Suma.RowCount = Fila1;
+            Suma.ColumnCount = Columna2;
+            
+                for (i = 0; i < Fila1; i++)
+                {
+                    for (j = 0; j < Columna2; j++)
+                    {
+                        Resultado[i, j] = MatrizA[i, j] + MatrizB[i, j];
+                        Suma.Rows[i].Cells[j].Value = Resultado[i, j].ToString();
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tienen que ser iguales las filas y columnas");
+            }
+           
+        }
+
+        public void Resta(DataGridView Resta)
+        {
+            if (Fila1 == Fila2 && Columna1 == Columna2)
+            {
+                Resta.RowCount = Fila1;
+                Resta.ColumnCount = Columna2;
+
+                for (i = 0; i < Fila1; i++)
+                {
+                    for (j = 0; j < Columna2; j++)
+                    {
+                        Resultado[i, j] = MatrizA[i, j] - MatrizB[i, j];
+                        Resta.Rows[i].Cells[j].Value = Resultado[i, j].ToString();
+
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tienen que ser iguales las filas y columnas");
+            }
+
+        }
+
+        public void Multiplicar(DataGridView Multiplica)
+        {
+            Multiplica.RowCount = Fila1;
+            Multiplica.ColumnCount = Columna2;
+
+            for(i=0; i<Fila1; i++)
+            {
+                for(j=0; j<Columna2; j++)
+                {
+                    Resultado[i, j] = 0;
+                    for(z=0; z<Columna1; z++)
+                    {
+                        Resultado[i,j]=(MatrizA[i,z]* MatrizB[z,j]) + Resultado[i, j];
+                        Multiplica.Rows[i].Cells[j].Value = Resultado[i,j].ToString();
+
+                    }
+                }
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
